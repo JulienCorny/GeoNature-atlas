edf8961d041820176682663b64b938627c768184
joel.clement <joelclems@gmail.com>
2023-06-16 15:07:52 +0200

    docker wip: reload

--- docker_startup.sh
+++ docker_startup.sh
@@ -1,21 +1,33 @@
 #!/bin/bash
+
+# script pour l'entry point du docker atlas
+
 set -eof pipefail
 export PGPASSWORD=${POSTGRES_PASSWORD}
 
-echo ATLAS_RESET_SCHEMA $ATLAS_RESET_SCHEMA
-
+# si la variable ATLAS_RESET_SCHEMA est à true
+# -> suppression en base de données des schema atlas & synthese
+#
 if [ "$ATLAS_RESET_SCHEMA" = true ];then
     psql -d ${POSTGRES_DB} -U ${POSTGRES_USER} -h ${POSTGRES_HOST} -p ${POSTGRES_PORT} -t -c "DROP SCHEMA IF EXISTS atlas CASCADE"
     psql -d ${POSTGRES_DB} -U ${POSTGRES_USER} -h ${POSTGRES_HOST} -p ${POSTGRES_PORT} -t -c "DROP SCHEMA IF EXISTS synthese CASCADE"
 fi
 
+
+# test si le schema atlas existe
 schema_atlas_exists=$(psql -d ${POSTGRES_DB} -U ${POSTGRES_USER} -h ${POSTGRES_HOST} -p ${POSTGRES_PORT} -t -c "SELECT exists(select schema_name FROM information_schema.schemata WHERE schema_name = 'atlas');" | sed 's/ //g')
+
+# si le schema atlas n'existe pas
 if [ ! "$schema_atlas_exists" = "t" ]; then
     echo "Schema atlas inexistant ($schema_atlas_exists)"
+
+    # si la variable ATLAS_INSTALL_SCHEMA est à true (ou ATLAS_RESET_SCHEMA)
+    # on procède à l'installation des schema atlas et synthese
     if [ "$ATLAS_INSTALL_SCHEMA" = true ] || [ "$ATLAS_RESET_SCHEMA" = true ] ;then
         echo Installation du schema de l''atlas
-        echo ATLAS_ALTITUDES $ATLAS_ALTITUDES
         ./docker_install_atlas_schema.sh
+
+    # sinon on  affiche un message avec les indication pour initier la base atlas
     else
         echo Pour installer la db avec cette commande veuillez definir la variable d''environnement ATLAS_INSTALL_SCHEMA=true
         exit 1
@@ -24,6 +36,11 @@ else
     echo Schema atlas déjà installé
 fi
 
-# lance l'application gunicorn
+# lancement l'application avec gunicorn
 echo Lancement de l''application atlas
-gunicorn "atlas.wsgi:create_app()" --name=geonature-atlas --bind=0.0.0.0:8080 --access-logfile=- --error-logfile=-
+gunicorn "atlas.wsgi:create_app()" \
+    --name=geonature-atlas \
+    --bind=0.0.0.0:8080 \
+    --access-logfile=- \
+    --error-logfile=- \
+    --reload # pour relancer l'application en cas de modification du fichier de configuration
