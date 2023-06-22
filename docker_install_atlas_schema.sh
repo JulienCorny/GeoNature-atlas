8507a6f8aaaf407a7d7b38d5ca965dc55141a3fc
joel.clement <joelclems@gmail.com>
2023-06-22 15:09:16 +0200

    script init schema atlas refresh

--- docker_install_atlas_schema.sh
+++ docker_install_atlas_schema.sh
@@ -75,5 +75,4 @@ psql -v ON_ERROR_STOP=1 -d ${POSTGRES_DB} -U ${POSTGRES_USER} -h ${POSTGRES_HOST
     -v type_maille=$ATLAS_TYPE_MAILLE \
     -f data/atlas/12.atlas.t_mailles_territoire.sql
 psql -v ON_ERROR_STOP=1 -d ${POSTGRES_DB} -U ${POSTGRES_USER} -h ${POSTGRES_HOST} -p ${POSTGRES_PORT} -f data/atlas/13.atlas.vm_observations_mailles.sql
-psql -v ON_ERROR_STOP=1 -d ${POSTGRES_DB} -U ${POSTGRES_USER} -h ${POSTGRES_HOST} -p ${POSTGRES_PORT} -f data/atlas/atlas.vm_refresh.sql
-# psql -v ON_ERROR_STOP=1 -d ${POSTGRES_DB} -U ${POSTGRES_USER} -h ${POSTGRES_HOST} -p ${POSTGRES_PORT} -f data/atlas/14.grant.sql
+psql -v ON_ERROR_STOP=1 -d ${POSTGRES_DB} -U ${POSTGRES_USER} -h ${POSTGRES_HOST} -p ${POSTGRES_PORT} -f data/atlas/atlas.refresh_materialized_view_data.sql
